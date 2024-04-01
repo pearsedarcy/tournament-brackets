84d2ceb8318e6d6591bfbed129f7b55259576126
Pearse Darcy <pearse.darcy@outlook.com>
2024-04-01 12:50:58 +0200

    feat: add function to create a new worksheet and prompt user for title of the tournament

--- run.py
+++ run.py
@@ -15,9 +15,25 @@ SHEET = GSPREAD_CLIENT.open('tournament_brackets')
 
 participants = SHEET.worksheet('participants')
 
+#create a new worksheet
+def create_worksheet(title):
+    '''
+    Creates a new worksheet with the given title
+    '''
+    SHEET.add_worksheet(title, 80, 25)
+
 # Main function
 def main():
+    '''
+    Main function
+    
+    '''
     print('Welcome to Tournament Brackets')
+    print('What would you like to call your new Tournament?')
+    tournament_title = input()
+    create_worksheet(tournament_title)
+    print(f'{tournament_title} has been created!')
+
 
 # Call main function
 if __name__ == '__main__':
