5421c0dbf74e96ba6dfe2ae3d9aae314253e6039
Pearse Darcy <pearse.darcy@outlook.com>
2024-04-05 12:51:19 +0200

    fix: indenting correctly for PEP8 linter

--- run.py
+++ run.py
@@ -16,7 +16,8 @@ SHEET = GSPREAD_CLIENT.open("tournament_brackets")
 
 def run_tournament(tournament_id):
     tournament_sheet = SHEET.worksheet(tournament_id)
-    participants = [participant[0] for participant in tournament_sheet.get("B2:B17")]
+    participants = [participant[0]
+                    for participant in tournament_sheet.get("B2:B17")]
     rounds = []
     print("\nGenerating Matches...\n")
     participants_copy = participants.copy()
@@ -31,7 +32,9 @@ def run_tournament(tournament_id):
             )
         participants_copy = run_matchups(participants_copy)
     winner = participants_copy[0]
-    SHEET.worksheet(tournament_id).batch_update([{"range": "J2", "values": [[winner]]}])
+    SHEET.worksheet(tournament_id).batch_update
+    ([{"range": "J2", "values": [[winner]]}])
+
     print("The winner of the tournament is:", winner)
     print("\n1. Return to Tournament Menu \n")
     print("2. Exit\n")
@@ -58,7 +61,8 @@ def run_match(participant1, participant2):
     winner = input(f"Winner: ").strip().title()
     while winner not in (participant1, participant2):
         print(
-            "Invalid input. Please enter the name of the winning participant correctly.\n"
+            "Invalid input."
+            "Please enter the name of the winning participant correctly.\n"
         )
         winner = input(f"Winner: ").strip().title()
     print(f"\nThe winner of the match is: {winner}\n")
@@ -83,21 +87,25 @@ def input_participants(tournament_id, tournament_title, size):
     print("For example: John, Jane, Doe, Smith\n")
     while True:
         participants = input("Participants:  ").split(",")
-        participants = [participant.strip().title() for participant in participants]
+        participants = [participant.strip().title()
+                        for participant in participants]
         if len(participants) != int(size):
             print(
-                f"\nInvalid number of participants. Please enter {size} participants\n"
+                f"\nInvalid number of participants."
+                "Please enter {size} participants\n"
             )
             continue
         if len(participants) != len(set(participants)):
             print("\nInvalid input. Participants cannot have the same name\n")
             continue
         if "" in participants:
-            print("\nInvalid input. Please enter the name of each participant\n")
+            print("\nInvalid input."
+                  "Please enter the name of each participant\n")
             continue
         break
     print(
-        f"\nPlease wait, Adding {len(participants)} participants to the tournament...\n"
+        f"\nPlease wait,"
+        "Adding {len(participants)} participants to the tournament...\n"
     )
     for i, participant in enumerate(participants, start=2):
         SHEET.worksheet(tournament_id).batch_update(
@@ -109,13 +117,16 @@ def input_participants(tournament_id, tournament_title, size):
 
 def import_participants(tournament_id, tournament_title, size):
     """
-    Import participants from the sample_participants sheet and add them to the tournament sheet
+    Import participants from the sample_participants sheet
+    and add them to the tournament sheet
     """
     sample_participants_sheet = SHEET.worksheet("sample_participants")
     sample_participants = sample_participants_sheet.get("B1:B" + size)
-    sample_participants_list = [participant[0] for participant in sample_participants]
+    sample_participants_list = [participant[0]
+                                for participant in sample_participants]
     print(
-        f"\nPlease wait, Adding {len(sample_participants_list)} participants to the tournament...\n"
+        f"\nPlease wait, Adding {len(sample_participants_list)}"
+        "participants to the tournament...\n"
     )
     for i, participant in enumerate(sample_participants_list, start=2):
         SHEET.worksheet(tournament_id).batch_update(
@@ -149,9 +160,9 @@ def view_tournament(tournament_id, tournament_title):
                 main()
             break
         else:
-            print(
-                '\nInvalid input. Please enter a valid option or type "Exit" to go back.\n'
-            )
+            print('\nInvalid input.'
+                  'Please enter a valid option or type "Exit" to go back.\n'
+                  )
 
 
 def choose_participants(tournament_id, tournament_title, size):
@@ -159,7 +170,8 @@ def choose_participants(tournament_id, tournament_title, size):
     Choose how to enter participants
 
     """
-    print("Would you like to enter your own participants or use sample participants?\n")
+    print("Would you like to enter your own participants"
+          "or use sample participants?\n")
     print("1. Enter participants manually")
     print("2. Use sample participants\n")
     print("Please enter the number of the option you would like to choose\n")
@@ -187,8 +199,9 @@ def create_tournament():
         tournament_title = input("Tournament Title:  ").title().strip()
         if len(tournament_title) > 3 and len(tournament_title) < 50:
             print(
-                f'\nAre you sure you want to use "{tournament_title}" as the title?\nYou will not be able to change this in the future (yes/no)\n'
-            )
+                f'\nAre you sure you want to use'
+                f'{tournament_title} as the title?\n'
+                'You will not be able to change this in the future(yes/no)\n')
             confirmation = input("Your Choice:  ").lower().strip()
             while confirmation.lower() not in ["yes", "y", "no", "n"]:
                 print('\nInvalid input. Please enter "Yes" or "No"\n')
@@ -199,10 +212,12 @@ def create_tournament():
                 print("\nPlease enter a new title\n")
         else:
             print(
-                "\nInvalid input. Please enter a title between 4 and 50 characters.\n"
+                "\nInvalid input."
+                "Please enter a title between 4 and 50 characters.\n"
             )
     # Create the ID of the tournament
-    tournament_id = tournament_title[0:3].upper() + str(random.randint(100, 999))
+    tournament_id = tournament_title[0:3].upper()
+    tournament_id += str(random.randint(100, 999))
     # check if the ID already exists and create a new one if it does
     while True:
         try:
@@ -221,7 +236,8 @@ def create_tournament():
     create_tournament_sheet(int(size), tournament_id)
     print(f"\n{tournament_title} has been created!\n")
     print(
-        f"The ID of the tournament is {tournament_id}\nPlease take note of this for future reference\n"
+        f"The ID of the tournament is {tournament_id}\n"
+        "Please take note of this for future reference\n"
     )
     choose_participants(tournament_id, tournament_title, size)
 
@@ -256,7 +272,8 @@ def main_menu(choice):
     if choice == "1":
         create_tournament()
     elif choice == "2":
-        print("\nPlease enter the ID of the Tournament you would like to view\n")
+        print(
+            "\nPlease enter the ID of the Tournament you would like to view\n")
         while True:
             tournament_id = input("Your Choice:  ").upper().strip()
             try:
@@ -267,15 +284,14 @@ def main_menu(choice):
                     SHEET.worksheet(tournament_id)
                 break
             except gspread.exceptions.WorksheetNotFound:
-                print(
-                    '\nInvalid ID. Please enter a valid ID or type "Exit" to go back\n'
-                )
+                print('\nInvalid ID.'
+                      'Please enter a valid ID or type "Exit" to go back\n')
         view_tournament(tournament_id, SHEET.worksheet(tournament_id).title)
     elif choice == "3":
         print("\nThank you for using Tournament Brackets\n")
-        print(
-            "If you would like to use the app again, please click the refresh button below\n"
-        )
+        print("If you would like to use the app again,"
+              "please click the refresh button below\n"
+              )
 
 
 def main():
